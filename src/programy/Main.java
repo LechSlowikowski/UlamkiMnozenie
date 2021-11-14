c3b05438559e6d4a03e1ccd31f6ab5e13b265156
Leszek Słowikowski <lslowikowski@gmail.com>
2021-11-14 13:49:20 +0100

    ver 002 - dodanie opidów funkcji

--- src/programy/Main.java
+++ src/programy/Main.java
@@ -8,7 +8,14 @@ public class Main {
     private static int licznik2;
     private static int mianownik2;
 
-    public static void wczytaj(){
+    /********************************************************
+     * nazwa funkcji: wczytaj
+     * parametry wejściowe: brak
+     * wartość zwracana: brak
+     * opis: wczytyje liczniki i mianowniki przemnażanych ułamków
+     * autor: 34567890123
+     * ****************************************************/
+    public static void wczytaj() {
         Scanner daneWejsciowe = new Scanner(System.in);
         System.out.print("Podaj licznik1: ");
         licznik1 = daneWejsciowe.nextInt();
@@ -23,17 +30,24 @@ public class Main {
         mianownik2 = daneWejsciowe.nextInt();
     }
 
+    /********************************************************
+     * nazwa funkcji: main
+     * parametry wejściowe: args - tablica parametrów uruchomienia
+     * wartość zwracana: brak
+     * opis: główna metoda uruchomieniowa programu
+     * autor: 34567890123
+     * ****************************************************/
     public static void main(String[] args) {
-	// write your code here
-    wczytaj();
+        // write your code here
+        wczytaj();
 
-    UlamkiMnozenie ulamkiMnozenie = new UlamkiMnozenie(licznik1,mianownik1, licznik2, mianownik2);
-    ulamkiMnozenie.przemnozUlamki();
+        UlamkiMnozenie ulamkiMnozenie = new UlamkiMnozenie(licznik1, mianownik1, licznik2, mianownik2);
+        ulamkiMnozenie.przemnozUlamki();
 
-    int iloczynLicznik = ulamkiMnozenie.getIloczynLicznik();
-    int iloczynMianownik = ulamkiMnozenie.getIloczynMianiownik();
+        int iloczynLicznik = ulamkiMnozenie.getIloczynLicznik();
+        int iloczynMianownik = ulamkiMnozenie.getIloczynMianiownik();
 
-    System.out.println(licznik1 + "/" + mianownik1 + " * " + licznik2 + "/" + mianownik2 + " = " + iloczynLicznik +"/" + iloczynMianownik);
+        System.out.println(licznik1 + "/" + mianownik1 + " * " + licznik2 + "/" + mianownik2 + " = " + iloczynLicznik + "/" + iloczynMianownik);
 
     }
 }
--- src/programy/UlamkiMnozenie.java
+++ src/programy/UlamkiMnozenie.java
@@ -1,7 +1,6 @@
 package programy;
 
 public class UlamkiMnozenie {
-
     private int licznik1;
     private int mianownik1;
     private int licznik2;
@@ -9,6 +8,16 @@ public class UlamkiMnozenie {
     private int iloczynLicznik;
     private int iloczynMianiownik;
 
+    /********************************************************
+     * nazwa funkcji: UlamkiMnozenie
+     * parametry wejściowe: licznik1 - licznik ułamka pierwszego
+     *                      mianiownik1 - mianownik ułamka pierwszego
+     *                      licznik2 - licznik ułamka drugiego
+     *                      int mianiownik2 - mianownik ułamka  drugiego
+     * wartość zwracana: brak
+     * opis: konstruktor klasy mnożącej ułamk
+     * autor: 34567890123
+     * ****************************************************/
     public UlamkiMnozenie(int licznik1, int mianiownik1, int licznik2, int mianiownik2) {
         this.licznik1 = licznik1;
         this.mianownik1 = mianiownik1;
@@ -16,8 +25,16 @@ public class UlamkiMnozenie {
         this.mianownik2 = mianiownik2;
     }
 
-    private int obliczNWD(int a, int b) {
 
+    /********************************************************
+     * nazwa funkcji: obliczNWD
+     * parametry wejściowe: a - pierwsza liczba
+     *                      b - druga liczba*
+     * wartość zwracana: największy wspólny dzielnik liczb a i b
+     * opis: oblicza największy wspólny dzielnik liczb a i b
+     * autor: 34567890123
+     * ****************************************************/
+    private int obliczNWD(int a, int b) {
         while (a != b) {
             if (a > b) {
                 a = a - b;
@@ -28,20 +45,39 @@ public class UlamkiMnozenie {
         return a;
     }
 
+    /********************************************************
+     * nazwa funkcji: przemnozUlamki
+     * parametry wejściowe: brak
+     * wartość zwracana: brak
+     * opis: przemnaża dwa ułamki zwykłe
+     * autor: 34567890123
+     * ****************************************************/
     public void przemnozUlamki() {
-
         iloczynLicznik = licznik1 * licznik2;
         iloczynMianiownik = mianownik1 * mianownik2;
         int nwd = obliczNWD(iloczynLicznik, iloczynMianiownik);
         iloczynLicznik = iloczynLicznik / nwd;
         iloczynMianiownik = iloczynMianiownik / nwd;
-
     }
 
+    /********************************************************
+     * nazwa funkcji: getIloczynLicznik
+     * parametry wejściowe: brak
+     * wartość zwracana: licznik ułamka będącego wynikiem mnożenia
+     * opis: zwraca licznik ułamka będącego wynikiem mnożenia
+     * autor: 34567890123
+     * ****************************************************/
     public int getIloczynLicznik() {
         return iloczynLicznik;
     }
 
+    /********************************************************
+     * nazwa funkcji: getIloczynMianiownik
+     * parametry wejściowe: brak
+     * wartość zwracana: mianownik ułamka będącego wynikiem mnożenia
+     * opis: zwraca mianownik ułamka będącego wynikiem mnożenia
+     * autor: 34567890123
+     * ****************************************************/
     public int getIloczynMianiownik() {
         return iloczynMianiownik;
     }
